c7a66afca81b66ab1f3a49bc5026295d8a15a2ef
wisniad@gmail.com <damiano1>
2018-01-13 19:37:37 +0100

    - authentication in progress

--- App/db/schema.rb
+++ App/db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180112144654) do
+ActiveRecord::Schema.define(version: 20180113180455) do
 
   create_table "user_lists", force: :cascade do |t|
     t.string "firstname"
     t.string "lastname"
     t.string "email"
     t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
