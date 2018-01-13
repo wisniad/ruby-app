f148446ca5f9905ed25c1711bac06564af1621a8
wisniad@gmail.com <damiano1>
2018-01-13 20:06:12 +0100

    - authentication completed

--- App/config/routes.rb
+++ App/config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   post 'signup' => 'users#create'
 
+  get 'login'  => 'sessions#new' 
+  delete 'logout' => 'sessions#destroy'
+  post 'login' => 'sessions#create'
+
   resources :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
